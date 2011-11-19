22cdaedc53efead1a9b902a0c51948f32e847208
Masamitsu MURASE <masamitsu.murase@gmail.com>
2011-11-20 04:32:45 +0900

    Add Compare Link to repository view.

--- lib/redmine_open_tortoise_svn_repository_view_hook.rb
+++ lib/redmine_open_tortoise_svn_repository_view_hook.rb
@@ -3,7 +3,14 @@
 module OpenTortoiseSvnHooks
   class OpenTortoiseSvnHooksViewListener < Redmine::Hook::ViewListener
     def view_repositories_show_contextual(context)
-      return "<h3><strong>Open TortoiseSVN test</strong></h3>"
+      return "<h3><strong>Open TortoiseSVN test</strong></h3>" +
+        context[:controller].send(:render_to_string, {
+                                    :partial => "open_tortoise_svn/repository_index",
+                                    :locals => {
+                                      :project => context[:project],
+                                      :repository => context[:repository]
+                                    }
+                                  })
     end
   end
 end
