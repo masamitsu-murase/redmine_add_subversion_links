39a8af7e97af3ac4e5c59d80a80332d57c5cbec3
Masamitsu MURASE <masamitsu.murase@gmail.com>
2011-11-20 19:27:40 +0900

    Add external links to "chages" and "show" page of repository view.

--- lib/redmine_open_tortoise_svn_repository_view_hook.rb
+++ lib/redmine_open_tortoise_svn_repository_view_hook.rb
@@ -2,15 +2,16 @@
 
 module OpenTortoiseSvnHooks
   class OpenTortoiseSvnHooksViewListener < Redmine::Hook::ViewListener
-    def view_repositories_show_contextual(context)
-      return "<h3><strong>Open TortoiseSVN test</strong></h3>" +
-        context[:controller].send(:render_to_string, {
-                                    :partial => "open_tortoise_svn/repository_index",
-                                    :locals => {
-                                      :project => context[:project],
-                                      :repository => context[:repository]
-                                    }
-                                  })
+    def view_layouts_base_html_head(context)
+      ctrl = context[:controller]
+      return "" unless (ctrl && ctrl.controller_name == "repositories" &&
+                        ctrl.instance_variable_defined?(:@project) &&
+                        ctrl.instance_variable_defined?(:@repository) &&
+                        ctrl.instance_variable_get(:@repository).scm_name == "Subversion")
+
+      return context[:controller].send(:render_to_string, {
+                                         :partial => "open_tortoise_svn/repository_index"
+                                       })
     end
   end
 end
