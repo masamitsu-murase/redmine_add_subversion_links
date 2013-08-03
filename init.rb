352d78b50a29df1f8c3d5d363faa1ad8e752e5b1
Masamitsu MURASE <masamitsu.murase@gmail.com>
2013-08-04 02:42:38 +0900

    Version 0.3.0

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_add_subversion_links do
   name 'Redmine Add Subversion Links'
   author 'Masamitsu MURASE'
   description 'This is a plugin for Redmine to add Subversion links to the repository.'
-  version '0.2.0'
+  version '0.3.0'
   url 'https://github.com/masamitsu-murase/redmine_add_subversion_links'
   author_url 'http://masamitsu-murase.blogspot.jp'
 end
